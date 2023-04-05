4c49deebd1674f1695910370f798e4fc8e7f9360
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-05 13:29:24 +0200

    Added randomized apple and collision detection

--- core/src/mao/linatrix/snake/GameScreen.java
+++ core/src/mao/linatrix/snake/GameScreen.java
@@ -7,49 +7,69 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class GameScreen extends ScreenAdapter {
 
 	private SpriteBatch batch;
-	private Texture img;
+	private Texture snakehead;
+	private Texture snakebody;
+	private Texture apple;
 	
-	private static final float MOVE_TIME = 0.5F;
+	private static final float MOVE_TIME = 0.3F;
 	private float timer = MOVE_TIME;
 	
 	private static final int SNAKE_MOVEMENT = 32;
 	private int snakeX = 0, snakeY = 0;
 	
+	private boolean appleAvailable = false;
+	private int appleX, appleY;
+	
 	private static final int RIGHT = 0;
 	private static final int LEFT = 1;
 	private static final int UP = 2;
 	private static final int DOWN = 3;
+	private static final int ESC = 4;
 	private int snakeDirection = RIGHT;
 	
 	@Override
 	public void show() {
 		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("snakehead.png"));
+		snakehead = new Texture(Gdx.files.internal("snakehead.png"));
+		snakebody = new Texture(Gdx.files.internal("snakebody.png"));
+		apple = new Texture(Gdx.files.internal("apple.png"));
 	}
 	
 	@Override
 	public void render(float delta) {
+		queryInput();
 		
 		timer -= delta;
 		if (timer <= 0) {
 			timer = MOVE_TIME;
 			//snakeX += SNAKE_MOVEMENT;
-			
 			moveSnake();
+			checkForOutOfBounds();
+			checkAppleCollision();
 		}
-			
-		checkForOutOfBounds();
-		queryInput();
 		
+		checkAndPlaceApple();
 		
+		clearScreen();
+		draw();
+	}
+	
+	private void clearScreen() {
 		Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	}
+	
+	private void draw() {
 		batch.begin();
-		batch.draw(img, snakeX, snakeY);
+		batch.draw(snakehead, snakeX, snakeY);
+		if (appleAvailable) {
+			batch.draw(apple, appleX, appleY);
+		}
 		batch.end();
 	}
 	
@@ -86,6 +106,8 @@ public class GameScreen extends ScreenAdapter {
 		case DOWN:
 			snakeY -= SNAKE_MOVEMENT;
 			return;
+		case ESC:
+			return;
 		}
 		
 	}
@@ -95,10 +117,29 @@ public class GameScreen extends ScreenAdapter {
 		boolean rPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 		boolean uPressed = Gdx.input.isKeyPressed(Input.Keys.UP);
 		boolean dPressed = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+		boolean escPressed = Gdx.input.isKeyPressed(Input.Keys.ESCAPE);
 		
 		if (lPressed) snakeDirection = LEFT;
 		if (rPressed) snakeDirection = RIGHT;
 		if (uPressed) snakeDirection = UP;
 		if (dPressed) snakeDirection = DOWN;
+		if (escPressed) Gdx.app.exit();
 	}
+	
+	private void checkAndPlaceApple() {
+		if (!appleAvailable) {
+			do {
+				appleX = MathUtils.random(Gdx.graphics.getWidth() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+				appleY = MathUtils.random(Gdx.graphics.getHeight() / SNAKE_MOVEMENT - 1) * SNAKE_MOVEMENT;
+				appleAvailable = true;
+			} while (appleX == snakeX && appleY == snakeY);
+		}
+	}
+	
+	private void checkAppleCollision() {
+		if (appleAvailable && appleX == snakeX && appleY == snakeY) {
+			appleAvailable = false;
+		}
+	}
+	
 }

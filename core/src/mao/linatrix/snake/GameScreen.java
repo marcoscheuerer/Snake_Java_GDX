3c3d6396e45327ac20a2327975989cdc4032cf96
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-04-05 09:08:33 +0200

    Added snakehead texture and event polling

--- core/src/mao/linatrix/snake/GameScreen.java
+++ core/src/mao/linatrix/snake/GameScreen.java
@@ -1,17 +1,104 @@
 package mao.linatrix.snake;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 
+	private SpriteBatch batch;
+	private Texture img;
+	
+	private static final float MOVE_TIME = 0.5F;
+	private float timer = MOVE_TIME;
+	
+	private static final int SNAKE_MOVEMENT = 32;
+	private int snakeX = 0, snakeY = 0;
+	
+	private static final int RIGHT = 0;
+	private static final int LEFT = 1;
+	private static final int UP = 2;
+	private static final int DOWN = 3;
+	private int snakeDirection = RIGHT;
+	
 	@Override
 	public void show() {
-	
+		batch = new SpriteBatch();
+		img = new Texture(Gdx.files.internal("snakehead.png"));
 	}
 	
 	@Override
 	public void render(float delta) {
-	
+		
+		timer -= delta;
+		if (timer <= 0) {
+			timer = MOVE_TIME;
+			//snakeX += SNAKE_MOVEMENT;
+			
+			moveSnake();
+		}
+			
+		checkForOutOfBounds();
+		queryInput();
+		
+		
+		Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, snakeX, snakeY);
+		batch.end();
 	}
 	
+	private void checkForOutOfBounds() {
+		if (snakeX >= Gdx.graphics.getWidth()) {
+			snakeX = 0;
+		}
+		
+		if (snakeX < 0) {
+			snakeX = Gdx.graphics.getWidth() - SNAKE_MOVEMENT;
+		}
+		
+		if (snakeY >= Gdx.graphics.getHeight()) {
+			snakeY = 0;
+		}
+		
+		if (snakeY < 0) {
+			snakeY = Gdx.graphics.getHeight() - SNAKE_MOVEMENT;
+		}
+		
+	}
+	
+	private void moveSnake() {
+		switch(snakeDirection) {
+		case RIGHT:
+			snakeX += SNAKE_MOVEMENT;
+			return;
+		case LEFT:
+			snakeX -= SNAKE_MOVEMENT;
+			return;
+		case UP:
+			snakeY += SNAKE_MOVEMENT;
+			return;
+		case DOWN:
+			snakeY -= SNAKE_MOVEMENT;
+			return;
+		}
+		
+	}
+	
+	private void queryInput() {
+		boolean lPressed = Gdx.input.isKeyPressed(Input.Keys.LEFT);
+		boolean rPressed = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+		boolean uPressed = Gdx.input.isKeyPressed(Input.Keys.UP);
+		boolean dPressed = Gdx.input.isKeyPressed(Input.Keys.DOWN);
+		
+		if (lPressed) snakeDirection = LEFT;
+		if (rPressed) snakeDirection = RIGHT;
+		if (uPressed) snakeDirection = UP;
+		if (dPressed) snakeDirection = DOWN;
+	}
 }
